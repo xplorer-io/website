cb762a4b36de3ddb596e4b3eb7833b04079f606e
Sonam Serchan <sonam.serchan9@gmail.com>
2024-06-17 18:22:44 +1000

    feat: issue-11 navigation (#15)
    * feat: add header
    
    * fix: format
    
    * fix: add key

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ export default function Home() {
     <main className="grid h-screen place-items-center">
       <div className="mx-auto max-w-3xl px-4">
         <div className="mb-4">
-          <h1 className="text-primary font-bold">Welcome to Xplorers!</h1>
-          <hr className="bg-secondary my-2 h-1 w-48 rounded border-0" />
+          <h1 className="font-bold text-primary">Welcome to Xplorers!</h1>
+          <hr className="my-2 h-1 w-48 rounded border-0 bg-secondary" />
         </div>
         <p>
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam eum
