b3f3254b34a78c6e555d6baf45d136b289787661
Sonam Serchan <sonam.serchan9@gmail.com>
2024-06-25 20:58:51 +1000

    feat: move all images into images folder

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import logo from "@public/logo.svg";
+import logo from "@public/images/logos/xplorer_logo.svg";
 import { usePathname } from "next/navigation";
 import clsx from "clsx";
 import { menus } from "./helper";
