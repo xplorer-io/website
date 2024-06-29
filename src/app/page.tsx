aa454468a296738dd4adb4849d4ef0e165355153
titungdup <dup.titung@gmail.com>
2024-06-29 17:27:01 +0930

    feat: add hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,14 @@
+import HeroSlider from "@/components/layout/heroSlider";
+import MarqueeBanner from "@/components/layout/marqueeBanner";
+import JoinXplorers from "@/components/layout/joinXplorers";
+import "swiper/css/bundle";
+
 export default function Home() {
   return (
-    <main className="grid h-screen place-items-center">
-      <div className="mx-auto max-w-3xl px-4">
-        <div className="mb-4">
-          <h1 className="font-bold text-primary">Welcome to Xplorers!</h1>
-          <hr className="my-2 h-1 w-48 rounded border-0 bg-secondary" />
-        </div>
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam eum
-          reiciendis accusantium itaque deleniti in ipsum fugiat eligendi
-          assumenda exercitationem dicta et perferendis, eveniet tempore libero
-          modi praesentium at placeat.
-        </p>
-      </div>
+    <main>
+      <HeroSlider />
+      <MarqueeBanner />
+      <JoinXplorers />
     </main>
   );
 }
