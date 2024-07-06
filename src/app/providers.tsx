33e00bbb851be16c7408870d4bf1bf79791a7700
Bishwas Adhikari <20wasadk@gmail.com>
2024-07-06 18:45:41 +1000

    fix: removed extra space

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -4,7 +4,6 @@ import React from 'react';
 import {AppContextProvider} from '@/context/AppContext';
 import {ClerkProvider} from "@clerk/nextjs";
 
-
 export function Providers({children}: { children: React.ReactNode }) {
     return (
         <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
