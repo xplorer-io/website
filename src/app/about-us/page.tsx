0058f7bd31ca504a70489aa8119511235825c19a
ghimireaacs <aacs.csu@gmail.com>
2024-07-13 23:44:49 +1000

    added feature OurValues for About US page

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -1,9 +1,11 @@
 import { OurMission } from "@/modules/about-us/OurMission";
+import { OurValues } from "@/modules/about-us/OurValues";
 
 export default function AboutUs() {
   return (
     <main>
       <OurMission />
+      <OurValues />
     </main>
   );
 }
