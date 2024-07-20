84bef8e82a48c0ab95ebac28a35d0cf292fade1f
ghimireaacs <aacs.csu@gmail.com>
2024-07-20 17:44:47 +1000

    modified card to component and resolved issues and merged main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import HeroSlider from "@/modules/home/heroSlider";
 import MarqueeBanner from "@/modules/home/marqueeBanner";
 import JoinXplorers from "@/modules/home/joinXplorers";
+import { CommunityBenefits } from "@/modules/home/communityBenefits/communityBenefits";
 import "swiper/css/bundle";
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
       <HeroSlider />
       <MarqueeBanner />
       <JoinXplorers />
+      <CommunityBenefits />
     </main>
   );
 }
