1082ca4554afd7f5057edae1f8a6ae21312a7c42
ghimireaacs <aacs.csu@gmail.com>
2024-07-28 16:09:09 +1000

    fixed type of CommunityBenefitCard

--- src/modules/home/communityBenefits/CommunityBenefitCard.tsx
+++ src/modules/home/communityBenefits/CommunityBenefitCard.tsx
@@ -1,6 +1,11 @@
 import Image from "next/image";
 
-export const CommunityBenefitCard = ({ name, imageurl }) => {
+type communityProps = {
+  name: string;
+  imageurl: string;
+};
+
+export const CommunityBenefitCard = ({ name, imageurl }: communityProps) => {
   return (
     <div key={name} className="flex flex-col items-center rounded-xl">
       <div className="flex h-96 w-80 flex-col items-center gap-4 rounded-md border-2 bg-blue-100 p-8 font-semibold text-black shadow-xl md:h-72 md:w-64 lg:h-[305px] lg:w-[256px]">
