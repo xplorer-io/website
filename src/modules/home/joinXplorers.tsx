fb9133f7cb79a0f87d5d03c7f93b5ab41072c089
Ashis Chaudhary <ashischaudhary959@gmail.com>
2024-07-30 18:45:26 +1000

    feat: add button animation

--- src/modules/home/joinXplorers.tsx
+++ src/modules/home/joinXplorers.tsx
@@ -1,14 +1,22 @@
 import { Button } from "@/components/ui/buttons/button";
+import slackIcon from "@public/images/icons/slack_logo.svg";
+import Image from "next/image";
+import css from "./joinXplorers.module.css";
+import { joinXplorersGoogleFormUrl } from "@/constants";
 
 const JoinXplorers = () => {
   return (
     <div className="flex justify-center px-5 py-10">
-      <Button
-        className="bg-primary-400 text-base hover:bg-primary md:text-xl dark:bg-primary-400 dark:text-white dark:hover:bg-primary"
-        size={"hero"}
-      >
-        Request to join Xplorers
-      </Button>
+      <a target="_blank" href={joinXplorersGoogleFormUrl}>
+        <Button
+          id={css.requestToJoinButton}
+          className="bg-primary-400 text-base hover:bg-primary md:text-xl dark:bg-primary-400 dark:text-white dark:hover:bg-primary"
+          size={"hero"}
+        >
+          <span className="mr-3"> Request to join Xplorers</span>
+          <Image src={slackIcon} alt="slack icon" width={30} height={30} />
+        </Button>
+      </a>
     </div>
   );
 };
