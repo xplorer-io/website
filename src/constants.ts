0a548e851ff339ee078d5d97b7ac4f6298277704
Ashis Chaudhary <ashischaudhary959@gmail.com>
2024-07-30 18:45:26 +1000

    feat: add button animation

---
+++ src/constants.ts
@@ -0,0 +1,5 @@
+export const joinSlackUrl =
+  "https://xplorers-io.slack.com/join/shared_invite/zt-27cdb5ouu-taJbzO_Z0E5kbdMQ8nAJ9A";
+
+export const joinXplorersGoogleFormUrl =
+  "https://docs.google.com/forms/d/e/1FAIpQLSfeCCGr01LcEMDkQIxmf7Oa3BNR1PQd33ymCDju79qmLpKXKA/viewform?usp=sf_link";
