4a187ce3dca19d9bc577168f6760d289f10476a0
ghimireaacs <aacs.csu@gmail.com>
2024-07-30 19:52:45 +1000

    resolved issues and fixed naming, increased modularity

--- src/modules/about-us/OurValues/helper.ts
+++ src/modules/about-us/OurValues/helper.ts
@@ -1,38 +1,43 @@
-export const ourValuesList = [
+export type OurValuesListProps = {
+  name: string;
+  description: string;
+  imageUrl: string;
+};
+export const ourValuesList: OurValuesListProps[] = [
   {
     name: "Community Engagement",
     description:
       "Community is in the heart of Xplorers in all endeavors, ensuring inclusivity, support, and empowerment for all members.",
-    imageurl: "./images/illustrations/earth.svg",
+    imageUrl: "./images/illustrations/earth.svg",
   },
   {
     name: "Collaboration",
     description:
       "Xplorers work together towards our shared goals, fostering a spirit of co-operation and teamwork.",
-    imageurl: "./images/illustrations/collaboration.svg",
+    imageUrl: "./images/illustrations/collaboration.svg",
   },
   {
     name: "Growth Mindset",
     description:
       "We nurture an environment that encourages continuous growth and development, both professionally and personally.",
-    imageurl: "./images/illustrations/Growth.svg",
+    imageUrl: "./images/illustrations/Growth.svg",
   },
   {
     name: "Sustainability",
     description:
       "We are committed to building tech community prioritizing long-term impact for individuals and community",
-    imageurl: "./images/illustrations/Async.svg",
+    imageUrl: "./images/illustrations/Async.svg",
   },
   {
     name: "Ownership",
     description:
       "Here at Xplorers, we take responsibility for individual actions, fostering accountability and initiative.",
-    imageurl: "./images/illustrations/rocketpc.svg",
+    imageUrl: "./images/illustrations/rocketpc.svg",
   },
   {
     name: "Curiosity",
     description:
       "We cultivate thirst for knowledge igniting innovation and fostering personal growth for every individual.",
-    imageurl: "./images/illustrations/curiosity.svg",
+    imageUrl: "./images/illustrations/curiosity.svg",
   },
 ];
--- src/modules/about-us/OurValues/ourValues.tsx
+++ src/modules/about-us/OurValues/ourValues.tsx
@@ -1,37 +1,24 @@
-"use client";
 import Image from "next/image";
-import { ourValuesList } from "./valuesHelper";
-
-type ourValuesProps = {
-  name: string;
-  description: string;
-  imageurl: string;
-};
+import { ourValuesList } from "./helper";
+import { OurValuesCard } from "./valuesCard";
 
 const OurValues = () => {
   return (
-    <>
-      <section className="flex flex-col justify-center gap-16 md:flex-col lg:mx-48">
-        <h2 className="text-center font-bold text-gray-600">Our Values</h2>
-        <section className="grid grid-cols-1 justify-between sm:gap-16 lg:grid-cols-3 lg:gap-12">
-          {ourValuesList.map(
-            ({ name, description, imageurl }: ourValuesProps) => (
-              <div key={name} className="flex flex-col justify-center">
-                <div className="flex flex-col items-center p-2 font-bold">
-                  <div className="flex justify-center lg:h-64 lg:w-64">
-                    <Image src={imageurl} width={250} height={120} alt={name} />
-                  </div>
-                  <span className="mt-4 text-center text-2xl">{name}</span>
-                  <span className="mt-4 text-center text-sm sm:mx-24 lg:mx-10">
-                    {description}
-                  </span>
-                </div>
-              </div>
-            ),
-          )}
-        </section>
+    <section className="flex flex-col justify-center gap-16 md:flex-col lg:mx-48">
+      <h2 className="text-center font-bold text-gray-600">Our Values</h2>
+      <section className="grid grid-cols-1 justify-between sm:gap-16 lg:grid-cols-3 lg:gap-12">
+        {ourValuesList.map(({ name, description, imageUrl }) => (
+          <div key={name} className="flex flex-col justify-center">
+            <OurValuesCard
+              key={name}
+              name={name}
+              imageUrl={imageUrl}
+              description={description}
+            />
+          </div>
+        ))}
       </section>
-    </>
+    </section>
   );
 };
 export default OurValues;
