0deb46a4662bcc27d61b1a30f252d81db73c53cc
Miraj Mainali <merazzmainali05@gmail.com>
2024-07-31 12:45:17 +1000

    Made the mobile nav responsive

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -10,8 +10,13 @@ import { usePathname } from "next/navigation";
 import { Button } from "@/components/ui/buttons/button";
 import navMobileIcon from "@public/images/illustrations/navMobileIcon.svg";
 
+import { useState } from "react";
+
 const Header = () => {
   const pathname = usePathname();
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const toggleMobileMenu = () => setIsMobileMenuOpen((prev) => !prev);
+
   return (
     <section className="container mx-auto px-4">
       <header className="bg-white">
@@ -47,12 +52,45 @@ const Header = () => {
                 </div>
 
                 <div className="block md:hidden">
-                  <Button className="rounded bg-gray-100 p-2 text-gray-600 transition hover:text-gray-600/75">
+                  <Button
+                    className="rounded bg-gray-100 p-2 text-gray-600 transition hover:text-gray-600/75"
+                    onClick={toggleMobileMenu}
+                  >
                     <Image src={navMobileIcon} alt={"Mobile Nav Icon"} />
                   </Button>
                 </div>
               </div>
             </div>
+          </div>
+          <div
+            className={clsx(
+              "overflow-hidden transition-all duration-150 ease-in-out",
+              {
+                "max-h-0": !isMobileMenuOpen,
+                "max-h-96": isMobileMenuOpen,
+              },
+            )}
+          >
+            <nav
+              aria-label="Mobile Global"
+              className="bg-white shadow-lg md:hidden"
+            >
+              <ul className="flex flex-col items-start gap-6 p-4 text-sm">
+                {menus.map(({ name, href }) => (
+                  <Link
+                    key={name}
+                    href={href}
+                    className={clsx({
+                      "text-gray-500 transition hover:text-gray-500/75": true,
+                      "border-b-2 border-gray-500/75": pathname === href,
+                    })}
+                    onClick={toggleMobileMenu}
+                  >
+                    {name}
+                  </Link>
+                ))}
+              </ul>
+            </nav>
           </div>
         </div>
       </header>
