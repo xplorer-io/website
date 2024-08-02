3a05fde3a4a0b0c54a87ad4c7d36eeeaeea7162c
Ashish <116858885+ghimireaacs@users.noreply.github.com>
2024-08-02 18:35:08 +1000

    Community engagement (#35)
    * added communty Benefits Card
    
    * changed tailwind config for shadow, UI Button and modified community Benefits section
    
    * Formatting and paths modified for consitency
    
    * Formatting and path modified for consitency
    
    * modified Tailwind config
    
    * fixed padding and code reviews
    
    * fixed padding and code reviews
    
    * fixed padding and code reviews
    
    * tried to fix dependency issues
    
    * changed Helper file position and reformatted structure
    
    * removed redundant files
    
    * modified card to component and resolved issues
    
    * modified card to component and resolved issues and merged main
    
    * resolved few issues including helpers, created community benefits list as components and fixed typos
    
    * fixed type of CommunityBenefitCard
    
    * imported types, fixed naming
    
    ---------
    
    Co-authored-by: Sonam Serchan <sonam.serchan9@gmail.com>

---
+++ src/modules/home/communityBenefits/helper.ts
@@ -0,0 +1,22 @@
+export type CommunityBenefitCardProps = {
+  name: string;
+  imageUrl: string;
+};
+export const communityBenefitsList: CommunityBenefitCardProps[] = [
+  {
+    name: "Community Engagement",
+    imageUrl: "./images/illustrations/group.svg",
+  },
+  {
+    name: "Learn and Share",
+    imageUrl: "./images/illustrations/share.svg",
+  },
+  {
+    name: "Networking Hub",
+    imageUrl: "./images/illustrations/networking.svg",
+  },
+  {
+    name: "Holistic Growth",
+    imageUrl: "./images/illustrations/helping.svg",
+  },
+];
