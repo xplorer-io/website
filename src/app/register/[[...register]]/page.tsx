7e6d56152a99b7d5fd707ca18b911dd6d9d748b7
Prasiddha Bista <42393225+boltdynamics@users.noreply.github.com>
2024-08-03 10:36:50 +1000

    feat(footer): shiny new footer for xplorers website (#41)
    * feat(footer): shiny new footer for xplorers website
    
    * feat(footer): align components on the bottom right side of the footer
    
    * feat(footer): use tailwind classes for css

--- src/app/register/[[...register]]/page.tsx
+++ src/app/register/[[...register]]/page.tsx
@@ -2,7 +2,7 @@ import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <div className="flex min-h-screen items-center justify-center">
+    <div className="flex items-center justify-center">
       <SignUp />
     </div>
   );
