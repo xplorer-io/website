485f0c3187785e33328278f20cc0fac5a90b64df
Prasiddha Bista <42393225+boltdynamics@users.noreply.github.com>
2024-08-03 10:36:50 +1000

    feat(footer): shiny new footer for xplorers website (#41)
    * feat(footer): shiny new footer for xplorers website
    
    * feat(footer): align components on the bottom right side of the footer
    
    * feat(footer): use tailwind classes for css

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    domains: ["via.placeholder.com"],
     remotePatterns: [
       {
         hostname: "images.unsplash.com",
