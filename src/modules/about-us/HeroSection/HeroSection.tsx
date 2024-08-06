a5c65834f3d65b488f29dbddbf163fa9cd8830f5
SajanMG <sajantamu04@gmail.com>
2024-08-06 19:31:50 +1000

    removed hidden in overflow in HeroSection page

--- src/modules/about-us/HeroSection/HeroSection.tsx
+++ src/modules/about-us/HeroSection/HeroSection.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 const HeroSection = () => {
   return (
-    <section className="overflow-hidden bg-gray-50 sm:grid sm:grid-cols-2 sm:items-center">
+    <section className="overflow bg-gray-50 sm:grid sm:grid-cols-2 sm:items-center">
       <div className="p-8 md:p-12 lg:px-16 lg:py-24">
         <TitleSection />
       </div>
