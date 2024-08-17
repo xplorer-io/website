9f759d332617ac49bdf6c50571bc4f57823635ab
SajanMG <sajantamu04@gmail.com>
2024-08-17 20:24:35 +1000

    feat:changed the image height and width

--- src/modules/home/communityBenefits/CommunityBenefitCard.tsx
+++ src/modules/home/communityBenefits/CommunityBenefitCard.tsx
@@ -8,7 +8,7 @@ export const CommunityBenefitCard = ({
   return (
     <div key={name} className="flex flex-col items-center rounded-xl bg-blue-100 shadow-xl border-2 p-8">
       <div className="flex flex-col items-center gap-4 font-semibold">
-        <div className="flex h-48 w-48 aspect-w-1 aspect-h-1 justify-center relative">
+        <div className="flex md:h-[10rem] md:w-[10rem] h-28 w-28 aspect-w-1 aspect-h-1 justify-center relative">
           <Image src={imageUrl} fill alt={name} className="object-contain"/>
         </div>
         <div className="mt-2 text-center text-lg">{name}</div>
