99ae20a832714c14afb8c7269edfe62541579a2b
SajanMG <sajantamu04@gmail.com>
2024-08-17 20:13:21 +1000

    feat:removed the inline height and width from Image element, added different sizes for mobile and desktop version of image

--- src/modules/about-us/OurValues/valuesCard.tsx
+++ src/modules/about-us/OurValues/valuesCard.tsx
@@ -8,8 +8,8 @@ export const OurValuesCard = ({
 }: OurValuesListProps) => {
   return (
     <div className="relative flex flex-col items-center p-2 font-bold">
-      <figure className="flex justify-center lg:h-64 lg:w-64">
-        <Image src={imageUrl} width={250} height={120} alt={name}/>
+      <figure className="relative flex justify-center h-[10rem] w-[10rem] lg:h-48 lg:w-48">
+        <Image src={imageUrl} fill alt={name}/>
       </figure>
       <span className="mt-4 text-center text-2xl">{name}</span>
       <span className="mt-4 text-center font-medium text-xs mx-12 lg:mx-10">
