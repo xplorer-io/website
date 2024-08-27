598a60f46f659fcc192568b962737b8813565956
Bishwas Adhikari <20wasadk@gmail.com>
2024-08-28 00:52:09 +1000

    fixed message error

--- src/modules/permission/NoPermissionPage.tsx
+++ src/modules/permission/NoPermissionPage.tsx
@@ -13,7 +13,7 @@ const NoPermissionPage = () => {
                     className="mx-auto h-56 w-auto text-black sm:h-64"
                 />
                 <h1 className="mt-6 text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl">Uh-oh!</h1>
-                <p className="mt-4 text-gray-500">You've stumbled into the no-permission zone 🚫🔐</p>
+                <p className="mt-4 text-gray-500">You&apos;ve stumbled into the no-permission zone 🚫🔐</p>
             </div>
         </div>
     )
