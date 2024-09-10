00425280ea83584a06b2c94fb905dfba52770847
Bishwas Adhikari <20wasadk@gmail.com>
2024-09-10 18:58:52 +1000

    hide protected route from menu

--- src/components/layout/MobileNavigation/MobileNav.tsx
+++ src/components/layout/MobileNavigation/MobileNav.tsx
@@ -1,44 +1,50 @@
 import React from "react";
 import clsx from "clsx";
-import { menus } from "../helper";
+import {menus} from "../helper";
 import MobileNavLinkItem from "./MobileNavLinkItem";
+import {useAppContext} from "@/context/AppContext";
 
 export interface MobileNavProps {
-  isMobileMenuOpen: boolean;
-  toggleMobileMenu: () => void;
-  pathname: string;
+    isMobileMenuOpen: boolean;
+    toggleMobileMenu: () => void;
+    pathname: string;
 }
 
 const MobileNav: React.FC<MobileNavProps> = ({
-  toggleMobileMenu,
-  isMobileMenuOpen,
-  pathname,
-}) => {
-  return (
-    <div
-      className={clsx(
-        "fixed right-0 top-0 z-50 h-full w-72 transform flex-col items-center justify-center bg-white shadow-lg transition-transform duration-300 ease-in-out md:hidden",
-        {
-          "translate-x-full": !isMobileMenuOpen,
-          "translate-x-0": isMobileMenuOpen,
-        },
-      )}
-    >
-      <nav aria-label="Mobile Global" className="p-2 text-sm">
-        <ul className="flex h-screen w-full flex-col items-center justify-center gap-4 p-3">
-          {menus.map((item) => (
-            <MobileNavLinkItem
-              href={item.href}
-              name={item.name}
-              key={item.name}
-              toggleMobileMenu={toggleMobileMenu}
-              pathname={pathname}
-            />
-          ))}
-        </ul>
-      </nav>
-    </div>
-  );
+                                                 toggleMobileMenu,
+                                                 isMobileMenuOpen,
+                                                 pathname,
+                                             }) => {
+    const {isActiveSlackUser = false} = useAppContext();
+
+    return (
+        <div
+            className={clsx(
+                "fixed right-0 top-0 z-50 h-full w-72 transform flex-col items-center justify-center bg-white shadow-lg transition-transform duration-300 ease-in-out md:hidden",
+                {
+                    "translate-x-full": !isMobileMenuOpen,
+                    "translate-x-0": isMobileMenuOpen,
+                },
+            )}
+        >
+            <nav aria-label="Mobile Global" className="p-2 text-sm">
+                <ul className="flex h-screen w-full flex-col items-center justify-center gap-4 p-3">
+
+                    {menus.map(({name, href, isProtected}) => (
+                        (!isProtected || isActiveSlackUser) && (
+                            <MobileNavLinkItem
+                                href={href}
+                                name={name}
+                                key={name}
+                                toggleMobileMenu={toggleMobileMenu}
+                                pathname={pathname}
+                            />
+                        )
+                    ))}
+                </ul>
+            </nav>
+        </div>
+    );
 };
 
 export default MobileNav;
