a5713d55f7fa83787d92a9930f6bac9ca2740e95
Miraj Mainali <merazzmainali05@gmail.com>
2024-10-04 00:48:08 +1000

    removed the test code

--- src/app/pathway-to-success/page.tsx
+++ src/app/pathway-to-success/page.tsx
@@ -5,14 +5,6 @@ import InfoCardTemplate from "@/components/ui/InfoCardTemplate";
 const PathwayToSuccess = () => {
   return (
     <div>
-      <InfoCardTemplate
-        description="lorenasdfjas sadlf lasfas flasf laksf lskflksajflask alskf lflkas flLorem ipsum dolor, sit amet consectetur adipisicing elit. Velit mollitia enim amet, maiores dicta eum ut cum quaerat iste facilis sunt, quidem deserunt neque iure. Corrupti debitis molestias temporibus hic!"
-        initialTitle="something"
-        highlightTitle="Summary Good"
-        number="5"
-        passingImage
-        link="https://wwww.facebook.com"
-      />
       <HeroSection />
       <CareerSuccessGuide />
     </div>
