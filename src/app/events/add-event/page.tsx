ba343bb1e0476687da4007d5e990a3a508528844
Pratibha <pratibhapaudel555@gmail.com>
2024-11-17 23:03:24 +1100

    Refact added metadata and changed variable to camecase

--- src/app/events/add-event/page.tsx
+++ src/app/events/add-event/page.tsx
@@ -1,5 +1,10 @@
 import { AddEventPage } from "@/modules/add-event";
-
+import { Metadata } from "next";
+export const metaData: Metadata = {
+  title: "Add Event - Create and Manage Your Events",
+  description:
+    "Easily add and manage your events with our user-friendly 'Add Event' page. Provide event details, upload images, and share them with your audience. Start creating events now!",
+};
 const AddEvent = () => {
   return <AddEventPage />;
 };
