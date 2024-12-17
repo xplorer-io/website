258cd33aec16ed00b78e79904b8034fd71a21e90
Ashis Chaudhary <ashischaudhary959@gmail.com>
2024-12-17 17:32:41 +1100

    add routes file

--- src/constants.ts
+++
@@ -1,6 +0,0 @@
-export const joinXplorersGoogleFormUrl =
-  "https://docs.google.com/forms/d/e/1FAIpQLSfeCCGr01LcEMDkQIxmf7Oa3BNR1PQd33ymCDju79qmLpKXKA/viewform?usp=sf_link";
-
-export const githubLink = "https://github.com/xplorer-io";
-export const linkedInLink = "https://www.linkedin.com/company/xplorersgroup";
-export const slackLink = "https://xplorers-io.slack.com/";
